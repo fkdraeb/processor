d21c4c0b955c9ad2b8571001209beaf40e3cefbf
Tiago Marques <tiago.g.marques.ext@spms.min-saude.pt>
2024-05-31 09:57:45 +0100

    new wsdl generation

--- src/main/java/pt/hip/soap/SoapService.java
+++ src/main/java/pt/hip/soap/SoapService.java
@@ -1,9 +1,19 @@
 package pt.hip.soap;
 
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebResult;
 import jakarta.jws.WebService;
+import jakarta.xml.ws.RequestWrapper;
+import jakarta.xml.ws.ResponseWrapper;
 
-@WebService
+@WebService(targetNamespace = "http://ws.connectors.connect.mirth.com/", name = "SoapService")
 public interface SoapService {
 
-    String acceptMessage (String soapMessage);
+    @WebMethod(action = "http://ws.connectors.connect.mirth.com/DefaultAcceptMessage/acceptMessageRequest")
+    @RequestWrapper(localName = "acceptMessage", targetNamespace = "http://ws.connectors.connect.mirth.com/", className = "com.connectors.ws.generated.AcceptMessage")
+    @ResponseWrapper(localName = "acceptMessageResponse", targetNamespace = "http://ws.connectors.connect.mirth.com/", className = "com.connectors.ws.generated.AcceptMessageResponse")
+    @WebResult(name = "return", targetNamespace = "")
+    String acceptMessage (@WebParam(name = "parameters", targetNamespace = "")
+                                  String soapMessage);
 }
--- src/main/java/pt/hip/soap/SoapServiceImpl.java
+++ src/main/java/pt/hip/soap/SoapServiceImpl.java
@@ -2,18 +2,36 @@ package pt.hip.soap;
 
 import jakarta.jws.WebService;
 
-import static pt.hip.soap.SoapMainProcessor.createGenericResponse;
-
-
-@WebService(endpointInterface = "pt.hip.soap.SoapService")
+//@WebService(endpointInterface = "pt.hip.soap.SoapService")
+@WebService(endpointInterface = "com.connectors.ws.generated.SoapService",
+        targetNamespace = "http://ws.connectors.connect.mirth.com/",
+        serviceName = "SoapServiceImpl")
 public class SoapServiceImpl implements SoapService {
 
-    @Override
+    /*@Override
     public String acceptMessage(String soapMessage) {
         //TODO Here should be created the logic to accept the message
 
-        //return "tesssssssssste";
-        return createGenericResponse("200", "", "1234", "OK");
+
+
+        return "The message was acknowledge";
+    }*/
+
+    @Override
+    public String acceptMessage(String parameters) {
+        // Implement your business logic here
+        // You can replace this with the desired response structure
+        return "<Response>" +
+                "<GenericResponse>" +
+                "<Error>" +
+                "<ErrorID>200</ErrorID>" +
+                "<ErrorMessage></ErrorMessage>" +
+                "</Error>" +
+                "<Response>OK</Response>" +
+                "<Search></Search>" +
+                "<ApplicationID>1234</ApplicationID>" +
+                "</GenericResponse>" +
+                "</Response>";
     }
 
 
