3b80dfda644eb255b51173a87c9e0cdcced0f0ae
Tiago Marques <tiago.g.marques.ext@spms.min-saude.pt>
2024-06-05 16:54:40 +0100

    adding security to soap endpoint

--- src/main/java/pt/hip/soap/SoapServiceImpl.java
+++ src/main/java/pt/hip/soap/SoapServiceImpl.java
@@ -1,72 +1,45 @@
 package pt.hip.soap;
 
-import jakarta.annotation.Resource;
 import jakarta.jws.WebService;
-import jakarta.xml.ws.WebServiceContext;
-import jakarta.xml.ws.handler.MessageContext;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
 
 @WebService(endpointInterface = "pt.hip.soap.SoapService")
 public class SoapServiceImpl implements SoapService {
 
     private String jdbcUrl = "jdbc:mysql://mysql:3306/HIP";
-    private String dbUsername = "root";
-    private String dbPassword = "password";
-
-    @Resource
-    private WebServiceContext wsctx;
+    private String username = "root";
+    private String password = "password";
 
     @Override
     public String acceptMessage(String soapMessage) {
 
-        MessageContext mctx = wsctx.getMessageContext();
+        /**TODO Here should be created the logic to create a new event
+         * Should validate the fields on the message
+         * Should validate the subscription is active
+         * More ...
+         * Should return is the event was created successfully, if not should be in the response info
+         */
 
-        // Get detail from request headers
-        Map<String, List<String>> http_headers = (Map<String, List<String>>) mctx.get(MessageContext.HTTP_REQUEST_HEADERS);
-        List<String> userList = http_headers.get("Username");
-        List<String> passList = http_headers.get("Password");
-
-        String username = "";
-        String password = "";
-
-        if (userList != null) {
-            // Get username
-            username = userList.get(0);
+        try {
+            saveMessageToDatabase(soapMessage);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
 
-        if (passList != null) {
-            // Get password
-            password = passList.get(0);
-        }
-
-        // Should validate username and password with database
-        if ("pis".equals(username) && "pis".equals(password)) {
-            return "Hello World JAX-WS - Valid User!";
-        } else {
-            return "Unknown User!";
-        }
-
-        // Uncomment to save the message to the database
-        // try {
-        //     saveMessageToDatabase(soapMessage);
-        // } catch (SQLException e) {
-        //     throw new RuntimeException(e);
-        // }
-        //
-        // return "The message was acknowledge";
+        return "The message was acknowledge";
     }
 
     private void saveMessageToDatabase(String message) throws SQLException {
         String sql = "INSERT INTO EVENTS_MESSAGES (EVENT_IN) VALUES (?)";
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUsername, dbPassword);
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
              PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, message);
             statement.executeUpdate();
         }
     }
+
 }
