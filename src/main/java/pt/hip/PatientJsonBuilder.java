58f146a6b3eeba2b6c69ab70e320e438ee91d7b6
Tiago Marques <tiago.g.marques.ext@spms.min-saude.pt>
2024-06-13 10:06:36 +0100

    changed the patientjsonbuilder

--- src/main/java/pt/hip/PatientJsonBuilder.java
+++ src/main/java/pt/hip/PatientJsonBuilder.java
@@ -7,19 +7,22 @@ import ca.uhn.hl7v2.parser.PipeParser;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import ca.uhn.hl7v2.model.Message;
+import org.apache.camel.util.json.JsonObject;
 
-public class SettingHeaderPatientId implements Processor {
+public class PatientJsonBuilder implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         String body = exchange.getIn().getBody(String.class);
+        JsonObject patientJson = new JsonObject();
 
         Message originMessage = hl7ParserFromString(body);
         MSH mshSegment = (MSH) originMessage.get("MSH");
         QPD qpdSegment = (QPD) originMessage.get("QPD");
 
         String patientId = qpdSegment.getQpd3_UserParametersInsuccessivefields().encode().replaceAll(".*\\^", "");
-        exchange.getIn().setHeader("patientId", patientId);
-        exchange.getIn().setHeader("originalMshId", mshSegment.getMsh10_MessageControlID().getValue());
+        patientJson.put("patientId",patientId);
+
+        exchange.getIn().setBody(patientJson);
     }
 
     public static Message hl7ParserFromString(String exchangeBody) throws HL7Exception {
